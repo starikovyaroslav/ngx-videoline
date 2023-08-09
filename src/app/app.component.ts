1ba6d21f2666851d829738f5ae50484dfbec23cc
yaroslav.starikov <yaroslav.starikov@eltex.loc>
2023-08-09 13:48:01 +0700

    remove ngx-video-timeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {VideoCellType} from 'ngx-timeliner';
+import {VideoCellType} from 'ngx-video-timeline';
 
 @Component({
   selector: 'app-root',
@@ -25,8 +25,15 @@ export class AppComponent implements OnInit {
     this.endTimeThreshold = new Date(new Date().getTime() + (3 * 3600 * 1000));
     this.videoCells = [
       {
-        beginTime: 1691082000000,
-        endTime: 1691168399000,
+        beginTime: 1691118007105,
+        endTime: 1691118240152,
+        style: {
+          background: '#2d9dff78'
+        }
+      },
+      {
+        beginTime: 1691118251228,
+        endTime: 1691118979589,
         style: {
           background: '#2d9dff78'
         }
